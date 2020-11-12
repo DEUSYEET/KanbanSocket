78018f4dac1aa661a999a0e4870a8b39216a9fa3
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2020-11-12 10:43:16 -0700

    aws urls

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
 // TODO update URL to be consistent with AWS
 // Uses socket.io.js (index.html) to make a connection with the server
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
+const socket = io('http://kanbansocket.us-west-1.elasticbeanstalk.com');
