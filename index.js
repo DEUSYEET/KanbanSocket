f1f3092852d9a9e3ae80b7b3d680369589063834
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2020-11-12 10:16:32 -0700

    basic server running

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-"File Created" 
+const express = require('express');
+const app = express();
+
+// Express piggy-backs off http or https. This allows Socket.IO integration
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const path = require('path');
+const port = 3000;
+
+// pass IO to server.js module
+
+server.listen(port, () => console.log(`Server listening on port ${port}`));
