f3808d29d2c19ea95fdbc11b1542be79ff7096a7
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2020-11-12 10:28:26 -0700

    Socket.IO can listen

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ const io = require('socket.io')(server);
 const path = require('path');
 const port = 3000;
 
-
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+  res.sendFile('index.html');
+});
 
-
-
-// pass IO to server.js module
+// pass IO to socket.js module
+const socket = require('./socket');
+socket(io);
 
 server.listen(process.env.PORT || port, () => console.log(`Server listening on port ${port}`));
