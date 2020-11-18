d0abc77ecb5017343100082c4d7f92c6acf0f600
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2020-11-18 10:06:17 -0700

    Used stuff from socket demo

--- socket.js
+++ socket.js
@@ -1,5 +1,12 @@
 module.exports = io => {
+    let users = {};
+
     io.on('connect', socket => {
-        console.log('new connection');
+        // console.log('new connection');
+
+        socket.on('user-connect', user => {
+            users[socket.id] = user;
+            io.sockets.emit('user-connect', `${user} connected`)
+        })
     });
 }
