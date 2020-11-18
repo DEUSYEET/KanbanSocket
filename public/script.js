0492ec4c394bcdf38a915a5e31f19c7b7a98f187
BrandinGoldsberry <bgoldsberry@student.neumont.edu>
2020-11-18 10:41:21 -0700

    Added custom username colors

--- public/script.js
+++ public/script.js
@@ -1,17 +1,22 @@
 // Uses socket.io.js (index.html) to make a connection with the server
-// const socket = io('http://localhost:3000');
-const socket = io('http://kanbansocket.us-west-1.elasticbeanstalk.com');
+const socket = io('http://localhost:3000');
+// const socket = io('http://kanbansocket.us-west-1.elasticbeanstalk.com');
 
 // HTML elements
 const messagesDOM = document.getElementById('message-container');
 const inputDOM = document.getElementById("message-input");
 const formDOM = document.getElementById('message-form');
 
+var userColor;
+
 let displayMessage = (user, message, local = false) => {
     let messageDOM = document.createElement('div');
 
     let userDOM = document.createElement('span');
-    if(local) userDOM.className = 'local';
+    if(local) { 
+        userDOM.className = 'local';
+        userDOM.style.color = userColor; 
+    }
     userDOM.innerText = user;
     messageDOM.append(userDOM);
 
@@ -33,6 +38,8 @@ let displayServerMessage = message => {
 // TODO replace with accounts?
 const user = prompt('Welcome! Please enter a username');
 socket.emit('user-connect', user);
+const color = prompt('Enter a hexcode color for your name!');
+userColor = color;
 
 formDOM.addEventListener('submit', evt => {
     evt.preventDefault();
@@ -47,7 +54,7 @@ formDOM.addEventListener('submit', evt => {
 
 //Socket.IO
 socket.on('chat-message', data => {
-    displayMessage(data.user,data.message);
+    displayMessage(data.user, data.message, data.user == user);
 })
 
 // Receive a plain message from the server
