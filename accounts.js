fc64977ef727ea2347786af554816e606889328d
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2020-11-18 11:24:30 -0700

    Login / Signup

---
+++ accounts.js
@@ -0,0 +1,82 @@
+let mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    "mongodb+srv://root:GRHSNOuDazb37S4j@guia0.evfpg.mongodb.net/guides?retryWrites=true&w=majority"
+  )
+  .catch((error) => console.log("error"));
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+var mdb = mongoose.connection;
+mdb.on("error", console.error.bind(console, "connection error:"));
+
+mdb.once("open", function (callback) {
+  console.log(mdb.name);
+});
+
+var User = new mongoose.Schema({
+  username: String,
+  password: String,
+  color:String
+});
+
+mongoose.model("KanbanUser", User);
+
+let KanbanUser = mongoose.model("KanbanUser");
+
+exports.createAccount = (req, res) => {
+  let newUser = {
+    username: req.body.username,
+    password: req.body.password,
+    color: req.body.color,
+  };
+  KanbanUser.findOneAndUpdate(
+    { username: newUser.username },
+    newUser,
+    { new: true, upsert: true },
+    (err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+};
+
+exports.getAccount = (req, res) => {
+  KanbanUser.findOne({ username: req.body.username }, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+};
+
+exports.login = (req, res) => {
+  let loginUser = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  KanbanUser.findOne({ username: loginUser.username }, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      if (result.password === loginUser.password) {
+        res.json({
+          login: true,
+          username: result.username,
+          color: result.color,
+        });
+      } else{
+        res.json({
+            login: false,
+          });
+      }
+    }
+  });
+};
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const accounts = require('./accounts');
+const bodyParser = require('body-parser');
+
 
 // Express piggy-backs off http or https. This allows Socket.IO integration
 const server = require('http').createServer(app);
@@ -9,10 +12,19 @@ const path = require('path');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
+app.get('/login', (req, res) => {
+  res.sendFile('login.html',{root:"public"});
+});
+
+app.route('/createAccount').post(accounts.createAccount);
+app.route('/getAccount').post(accounts.getAccount);
+app.route('/loginAccount').post(accounts.login);
 
 // pass IO to socket.js module
 const socket = require('./socket');
